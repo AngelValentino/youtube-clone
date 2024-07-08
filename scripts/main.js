3868f73df56967db83efe6ce8b68755ce9046ed6
AngelValentino <138892399+AngelValentino@users.noreply.github.com>
2024-07-09 01:14:24 +0200

    Refactored side menu, added toggle side menu link functionality.

--- scripts/main.js
+++ scripts/main.js
@@ -7,6 +7,7 @@ const rightSearchWithVoiceBtn = document.getElementById('navigation-bar-right__s
 const middleSearchWtichVoiceBtn = document.getElementById('navigation-bar-middle__search-with-voice-btn');
 const navbarSettingsBtn = document.getElementById('navigation-bar-right__settings-btn');
 const settingsModalLm = document.getElementById('settings-modal');
+const sideMneuLinksLms = document.querySelectorAll('.side-menu__link');
 
 settingsModalLm.innerHTML = settingsModalData.map(({ icon, title, chevron }) => (
   icon !== 'settings' 
@@ -27,6 +28,24 @@ settingsModalLm.innerHTML = settingsModalData.map(({ icon, title, chevron }) => 
       `
 )).join('');
 
+
+function setSideMenuLinkActiveClass() {
+  const currentHash = window.location.hash;
+
+  sideMneuLinksLms.forEach(sideMenuLink => {
+    const sideMenuHref = sideMenuLink.getAttribute('href');
+
+    if (sideMenuHref === currentHash || (sideMenuHref === '/' && currentHash === '')) {
+      sideMenuLink.classList.add('active');
+    } 
+    else {
+      sideMenuLink.classList.remove('active');
+    }
+  });
+}
+
+
+
 // Add modal events
 navbarSettingsBtn.addEventListener('click', handleToggleModalSettings);
 middleSearchWtichVoiceBtn.addEventListener('click', openSearchWithVoiceModal);
@@ -34,3 +53,15 @@ rightSearchWithVoiceBtn.addEventListener('click', openSearchWithVoiceModal);
 
 // Add event listeners to open search bar
 openSearchBarBtn.addEventListener('click', openSearchBar);
+
+// Add event listeners to side menu links
+sideMneuLinksLms.forEach(sideMenuLink => {
+  sideMenuLink.addEventListener('click', e => {
+    e.preventDefault(); // Prevent default anchor behavior
+    const hash = sideMenuLink.getAttribute('href');
+    history.replaceState(null, null, hash); // Update the URL hash without triggering scroll
+    setSideMenuLinkActiveClass(); // Set active class based on clicked item
+  });
+});
+
+setSideMenuLinkActiveClass(); // Set active side menu class on the first page load
